1948f861c1bf80c2ac92a260e30ee59e25d6d78b
Ioana Bostan <ioana.bostan@iquestgroup.com>
2019-09-09 16:47:05 +0300

    character details component finished

--- src/app/characters/character/character.component.ts
+++ src/app/characters/character/character.component.ts
@@ -1,16 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ICharacter } from '../character';
+import { CharacterService } from '../character.service';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-character',
   templateUrl: './character.component.html',
   styleUrls: ['./character.component.css']
 })
-export class CharacterComponent implements OnInit {
-  name: string = 'Default Name';
+export class CharacterComponent implements OnInit, OnDestroy {
+  pageTitle: string ='Character details';
+  character: ICharacter;
+  private subscription: Subscription = new Subscription();
   
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private characterService: CharacterService,
+    private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.subscription.add(this.characterService.getCharacter(id)
+      .subscribe({
+        next: character => this.character = character
+      })
+    );
   }
 
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+   
 }
