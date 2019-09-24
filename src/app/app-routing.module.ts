0e5209bbfffd7c5455351da004b1c08ec0195990
Ioana Bostan <ioana.bostan@iquestgroup.com>
2019-09-24 14:29:40 +0300

    custom preloading strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AuthGuard } from './users/auth.guard';
+import { SelectiveStrategy } from './selective-strategy.service';
 
 const routes = [
   { path: '', component: HomeComponent },
   {
-    path: '',
+    path: 'characters',
+    canActivate: [AuthGuard],
+    data: { preload: true },
+    //asynchronously load the specified file on demand
     loadChildren: () =>
+      //import returns a promise, when the request is complete, the promise is resolved and we
+      //specify the angular module to load from that file
       import('./characters/character.module').then(m => m.CharacterModule)
   },
   { path: '**', redirectTo: '' }
@@ -17,7 +24,7 @@ const routes = [
   declarations: [],
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, { preloadingStrategy: SelectiveStrategy })
   ],
   exports: [
     RouterModule
